b9c299fcbee576522c795433b9003c4fd36aa175
yun <ddr04014@gmail.com>
2023-10-27 20:54:51 +0900

    [feat] set prod deployments

--- chatnote/settings/prod.py
+++ chatnote/settings/prod.py
@@ -22,25 +22,22 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DEV_AWS_DB_NAME"),
-        "USER": os.getenv("DEV_AWS_DB_USER"),
-        "PASSWORD": os.getenv("DEV_AWS_DB_PASSWORD"),
-        "HOST": os.getenv("DEV_AWS_DB_HOST"),
-        "PORT": os.getenv("DEV_AWS_DB_PORT"),
+        "NAME": os.getenv("PROD_AWS_DB_NAME"),
+        "USER": os.getenv("PROD_AWS_DB_USER"),
+        "PASSWORD": os.getenv("PROD_AWS_DB_PASSWORD"),
+        "HOST": os.getenv("PROD_AWS_DB_HOST"),
+        "PORT": os.getenv("PROD_AWS_DB_PORT"),
     },
 }
 
-ES_USER = os.getenv("DEV_ES_USER")
-ES_PASSWORD = os.getenv("DEV_ES_PASSWORD")
-ES_CLOUD_ID = os.getenv("DEV_ES_CLOUD_ID")
+ES_USER = os.getenv("ES_USER")
+ES_PASSWORD = os.getenv("ES_PASSWORD")
+ES_CLOUD_ID = os.getenv("ES_CLOUD_ID")
 
-CHUNKED_DOCUMENT_INDEX = os.getenv("DEV_CHUNKED_DOCUMENT_INDEX")
+CHUNKED_DOCUMENT_INDEX = os.getenv("PROD_CHUNKED_DOCUMENT_INDEX")
 
-NOTION_CLIENT_ID = os.getenv("DEV_NOTION_CLIENT_ID")
-NOTION_SECRET = os.getenv("DEV_NOTION_SECRET")
-NOTION_REDIRECT_URL = os.getenv("DEV_NOTION_REDIRECT_URL")
-NOTION_AUTH_URL = os.getenv("DEV_NOTION_AUTH_URL")
+NOTION_CLIENT_ID = os.getenv("NOTION_CLIENT_ID")
+NOTION_SECRET = os.getenv("NOTION_SECRET")
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_CALLBACK_URI = os.getenv("GOOGLE_REDIRECT_URL")
